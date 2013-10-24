316e6193af1af9bb20d414828f0a699bd66117c2
Akos Vandra <avandra@whitepages.com>
2013-10-24 17:04:16 +0200

    now forwarding any parameters as well

--- app/controllers/mavatar/image_controller.rb
+++ app/controllers/mavatar/image_controller.rb
@@ -3,7 +3,7 @@ require_dependency "mavatar/application_controller"
 module Mavatar
   class ImageController < ApplicationController
     def show
-       redirect_to "http://www.gravatar.com/avatar/#{params[:hash]}"
+       redirect_to "http://www.gravatar.com/avatar/#{params[:hash]}", params
     end
   end
 end
